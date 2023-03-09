df04ffa838d9970fcfaa24cb0098e5f96666bccf
Chandan Ojha <chandanojha776@gmail.com>
2023-03-09 14:32:38 +0600

    `using-unions-to-run-queries-independently`

--- app/Models/User.php
+++ app/Models/User.php
@@ -50,13 +50,21 @@ class User extends Authenticatable
     public function scopeSearch($query, string $terms = null)
     {
         collect(str_getcsv($terms, ' ', '"'))->filter()->each(function ($term) use ($query) {
-            $term = $term . '%';
-            $query->where(function ($query) use ($term) {
-                $query->where('name', 'like', $term)
-                       ->orWhereIn('company_id',Company::query()
-                           ->where('name','like',$term)
-                           ->pluck('id')
-                       );
+            $term = $term.'%';
+            $query->whereIn('id',function ($query) use ($term){
+                $query->select('id')
+                    ->from(function ($query) use ($term){
+                        $query->select('id')
+                            ->from('users')
+                            ->where('name','like',$term)
+                            ->union(
+                                $query->newQuery()
+                                      ->select('users.id')
+                                      ->from('users')
+                                      ->join('companies','companies.id','=','users.company_id')
+                                      ->where('companies.name','like',$term)
+                            );
+                    },'matches');
             });
         });
     }
