1fe5b3714d5a747920c4fde8853867952cbef01d
Chandan Ojha <chandanojha776@gmail.com>
2023-03-09 15:03:43 +0600

    `fuzzy-searching`

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,22 +47,23 @@ class User extends Authenticatable
         return $this->belongsTo(Company::class);
     }
 
-    public function scopeSearch($query, string $terms = null)
+    public function scopeSearch($query, string $terms =null)
     {
-        collect(str_getcsv($terms, ' ', '"'))->filter()->each(function ($term) use ($query) {
-            $term = $term.'%';
+        collect(str_getcsv($terms, '', '"'))->filter()->each(function ($term) use ($query) {
+            $term = preg_replace('/[^A-Za-z0-9]/','',$term).'%';
             $query->whereIn('id',function ($query) use ($term){
                 $query->select('id')
                     ->from(function ($query) use ($term){
                         $query->select('id')
                             ->from('users')
-                            ->where('name','like',$term)
+                            //->where('name','like',$term)
+                            ->whereRaw("regexp_replace(name,'[^A-Za-z0-9]', '') like ?",[$term])
                             ->union(
                                 $query->newQuery()
                                       ->select('users.id')
                                       ->from('users')
                                       ->join('companies','companies.id','=','users.company_id')
-                                      ->where('companies.name','like',$term)
+                                      ->whereRaw("regexp_replace(companies.name,'[^A-Za-z0-9]','') like ?",[$term])
                             );
                     },'matches');
             });
